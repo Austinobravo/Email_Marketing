ddbe0c21cf3491b13ec728304d01a109f7c2d689
Ebuka <austinobravo@gmail.com>
2023-09-07 16:19:05 +0100

    Removed custom.js

--- email_marketing_app/urls.py
+++ email_marketing_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import custom_message, index
 
 urlpatterns = [
-    path('', custom_message, name='custom_message'),
+    path('', custom_message, name='home'),
     path('success/', index, name='index'),
 ]
