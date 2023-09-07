9183dc8879da86741aef865e1d7017a6f59056a5
Ebuka <austinobravo@gmail.com>
2023-09-07 15:31:15 +0100

    Removed tinymce

--- email_marketing/urls.py
+++ email_marketing/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("email_marketing_app.urls")),
     path('summernote/', include('django_summernote.urls')),
-    path('tinymce/', include('tinymce.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
